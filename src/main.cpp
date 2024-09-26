b1c730b6800b74a62f5db2c591d1dde98cda755d
AuroraChasma <astraignis1@gmail.com>
2024-09-26 17:14:08 +0500

    creating window

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,26 @@
 #include <SDL2/SDL.h>
 #endif
 
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-int main() {
-	int x = 5;
-	int y = 6;
-	std::cout << "Hello World!";
+int main(int arc, char ** argv) {
+
+    if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+    } else {
+        
+        SDL_CreateWindow(
+            "SDL2 Demo",
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            SCREEN_WIDTH, SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN
+        );
+        
+        SDL_Delay(2000);
+    }
+    
+    return 0;
 }
 
